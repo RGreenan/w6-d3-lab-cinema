8dbf155709c06df78b827a2859467240a9313a70
Rob Wilson <rob.wilson195@gmail.com>
2018-11-21 13:19:21 +0000

    Started testing cinema.

---
+++ models/cinema.js
@@ -0,0 +1,12 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+
+Cinema.prototype.filmTitles = function () {
+  return this.films.map((film) => {
+    return film.title;
+  })
+};
+
+module.exports = Cinema;
