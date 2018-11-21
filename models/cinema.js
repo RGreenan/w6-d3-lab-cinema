92c403b920439e5dc1ed4c7aa72d530239ead23c
Rob Wilson <rob.wilson195@gmail.com>
2018-11-21 13:44:48 +0000

    Completed up until 'no films'

--- models/cinema.js
+++ models/cinema.js
@@ -9,4 +9,22 @@ Cinema.prototype.filmTitles = function () {
   })
 };
 
+Cinema.prototype.filmByTitle = function (filmName) {
+  return this.films.find((film) => {
+    return film.title === filmName;
+  })
+};
+
+Cinema.prototype.filmsOfGenre = function (filmGenre) {
+  return this.films.filter((film) => {
+    return film.genre === filmGenre;
+  })
+};
+
+Cinema.prototype.checkFilmsFromYear = function (releaseYear) {
+  return this.films.some((film) => {
+    return film.year === releaseYear;
+  })
+};
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -32,10 +32,20 @@ describe('Cinema', function () {
     const actual = cinema.filmTitles();
     assert.deepStrictEqual(actual, ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting'])
   });
-  xit('should be able to find a film by title');
-  xit('should be able to filter films by genre');
-  xit('should be able to check whether there are some films from a particular year');
-  xit('should be able to check whether there are no films from a particular year');
+
+  it('should be able to find a film by title', function () {
+    const actual = cinema.filmByTitle('Black Panther');
+    assert.deepStrictEqual(actual, blackPanther);
+  });
+  it('should be able to filter films by genre', function () {
+    const actual = cinema.filmsOfGenre('drama');
+    assert.deepStrictEqual(actual, [moonlight, trainspotting]);
+  });
+  it('should be able to check whether there are some films from a particular year', function () {
+    const actual = cinema.checkFilmsFromYear(2017);
+    assert.strictEqual(actual, true);
+  });
+  it('should be able to check whether there are no films from a particular year');
   xit('should be able to check whether all films are over a particular length');
   xit('should be able to calculate total running time of all films');
 
